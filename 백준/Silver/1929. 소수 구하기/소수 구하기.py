045907bbbd0ffddbdf9fc1f46dc10d789ad27ae3
SongDerrick <96987446+SongDerrick@users.noreply.github.com>
2023-01-20 13:24:14 +0900

    [Silver III] Title: 소수 구하기, Time: 1052 ms, Memory: 116060 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+m, n = map(int,input().split())
+for i in range(m,n+1):
+    if i > 1:
+        sosu = True
+        max = int(i ** 0.5)
+        for j in range(2,max+1):
+            if i % j == 0:
+                sosu = False
+                break
+        if sosu:
+            print(i)
