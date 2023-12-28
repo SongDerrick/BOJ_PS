d9f1364be707c9aea0d7d5cae71f6cc68942a3fb
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-12-29 00:38:52 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 54996 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -6,8 +6,8 @@ N = int(input())
 
 card = deque([i for i in range(1, N + 1)])
 
-while len(card) > 1:
-  card.popleft()
-  card.append(card.popleft())
+while len(card) > 1:  # 카드 한 장 남을 때까지 반복
+  card.popleft()  # 일단 가장 아래 카드 뺌
+  card.append(card.popleft())  # 그 다음 카드 빼서 더함
 
-print(card.pop())
+print(card.pop())  # 마지막 남은 카드
